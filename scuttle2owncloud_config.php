50ba0db3a6e415ccbc3c67a18a0e60ffd038346d
iksiksiks <iksiksiks@users.noreply.github.com>
2019-10-16 16:39:46 +0200

    Update scuttle2owncloud_config.php ; modify
    Added datetime vars

--- scuttle2owncloud_config.php
+++ scuttle2owncloud_config.php
@@ -2,9 +2,10 @@
 /**
 * scuttle2owncloud_config.php
 * released under MIT License(see extra file)
-* version 0.9.1
+* version 0.9.1a
 * (c) by repat, <repat[at]repat[dot]de>, http://repat.de
 * June 2013
+* Forked and modified by iksiksiks in October 2019; added datetime parsing
 */
 
 // Enter user(name), password and database.
@@ -32,5 +33,6 @@ $TAG = 1;
 $TITLE = 1;
 $URL = 2;
 $DESCRIPTION = 3;
-
+$DATETIME = 4;
+$MODIFIED = 5;
 ?>
